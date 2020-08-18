d3818291141000641cac59ab7aa5321d0d65ad0d
dongpu1813 <42663880+dongpu1813@users.noreply.github.com>
2020-08-19 06:52:46 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,7 +26,7 @@ if [ ! -d $HOME/multipool ]; then
 	echo Downloading MultiPool Installer ${TAG}. . .
 	git clone \
 		-b ${TAG} --depth 1 \
-		https://github.com/yonnn/multipool_setup.git \
+		https://github.com/yonnn/multipool_setup \
 		"$HOME"/multipool/install \
 		< /dev/null 2> /dev/null
 
